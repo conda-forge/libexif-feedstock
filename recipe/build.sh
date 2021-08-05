9c4ab47f286b3d8e3d9de492bc857bb9424b320d
Isuru Fernando <isuruf@gmail.com>
2021-08-05 18:51:53 +0530

    Fix unbounded

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 ./configure --prefix=${PREFIX}
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  make check
 fi
 make install
